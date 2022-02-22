bdb62f4338b931e14dc46a9371b680629cc6a0da
ialexpovod <ialexefimenko@icloud.com>
2022-02-22 15:39:05 +0300

    additional edited

--- ATOMTEXSPECTR/spectr.py
+++ ATOMTEXSPECTR/spectr.py
@@ -5,7 +5,7 @@ import datetime
 import numpy
 from ATOMTEXSPECTR import plotting
 from ATOMTEXSPECTR.utility import sqrt_bins, bin_centers_from_edges, handle_datetime, handle_uncertain, ALL_UFloats, machineEpsilon
-import ATOMTEXSPECTR.read
+import ATOMTEXSPECTR
 from uncertainties import UFloat, unumpy
 
 
